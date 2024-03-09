51f61276c3e20d12f504a3a3ae0732792044bee5
Mihir Katyal <mkatyal@uci.edu>
2024-03-08 20:42:54 -0800

    added def generate template

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+
+def generate_temperature_graph(data, city_name):
+    temperatures = [data_point['main']['temp'] for data_point in data]
+    dates = [data_point['dt'] for data_point in data]  
+
+plt.figure(figsize=(10, 5))
+plt.plot(dates, temperatures, marker='o')
+plt.title(f'Temperature Trend for {city_name}')   
